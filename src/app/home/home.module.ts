30800929d6dcaa3c95685c184a5c2aa6c5790b1f
Javier Ocampo <info@javierocampo.net>
2023-08-12 12:17:58 +0200

    add calendar

---
+++ src/app/home/home.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { IonicModule } from '@ionic/angular';
+import { FormsModule } from '@angular/forms';
+import { HomePage } from './home.page';
+
+import { HomePageRoutingModule } from './home-routing.module';
+import { NgCalendarModule } from 'ionic7-calendar';
+
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    HomePageRoutingModule,
+    NgCalendarModule
+  ],
+  declarations: [HomePage]
+})
+export class HomePageModule {}
