e1ebc08436e749553ad7b44571cdc0942e5033bc
Javier Ocampo <info@javierocampo.net>
2023-08-13 08:50:56 +0200

    add arrows

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { FormsModule } from '@angular/forms';
@@ -7,6 +7,9 @@ import { HomePage } from './home.page';
 import { HomePageRoutingModule } from './home-routing.module';
 import { NgCalendarModule } from 'ionic7-calendar';
 
+import { registerLocaleData } from '@angular/common';
+import localeDe from '@angular/common/locales/de';
+registerLocaleData(localeDe, 'de-DE')
 
 @NgModule({
   imports: [
@@ -16,6 +19,7 @@ import { NgCalendarModule } from 'ionic7-calendar';
     HomePageRoutingModule,
     NgCalendarModule
   ],
-  declarations: [HomePage]
+  declarations: [HomePage],
+  providers: [{ provide: LOCALE_ID, useValue: 'de-DE' }]
 })
 export class HomePageModule {}
